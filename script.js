796c85881002c7f28d6aa784ee81dd3209ac561b
bagussatoto <bagusbudisatoto2001@gmail,com>
2023-02-12 02:17:43 +0700

    Tuker.in

---
+++ script.js
@@ -0,0 +1,71 @@
+//navbar fixed
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixedNav= header.offsetTop;
+    if(window.pageYOffset > fixedNav){
+        header.classList.add('navbar-fixed');
+    }
+    else {
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+//Transfer button
+const transfer = document.querySelector('#transfer');
+const pay = document.querySelector('#pay');
+const viewTrans = document.querySelector('#viewTrans');
+const exit = document.querySelector('#exit');
+
+transfer.addEventListener('click', function() {
+    viewTrans.classList.toggle('hidden');
+});
+
+exit.addEventListener('click', function() {
+    viewTrans.classList.toggle('hidden');
+});
+
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+const sideBar = document.querySelector('#sidebar');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+
+hamburger.addEventListener('click', function() {
+    sideBar.classList.toggle('left-[-300px]');
+});
+
+//submit
+document.getElementById("submit").onclick = function () {
+    page.href = "#index.html";
+};
+
+// Modal Popup-btn
+window.addEventListener('DOMContentLoaded',() =>{
+    const passwd = document.querySelector('#passwd')
+    const payBtn = document.querySelector('#pay-btn')
+    const confirmBtn = document.querySelector('#confirm-btn')
+    const closeBtn = document.querySelector('#close-modal')
+
+    const toggleModal = () => {
+        passwd.classList.toggle('hidden');
+        passwd.classList.toggle('flex');
+    }
+    payBtn.addEventListener('click', toggleModal)
+    closeBtn.addEventListener('click', toggleModal)
+    confirmBtn.addEventListener('click',() => {
+        passwd.classList.add('hidden')
+        passwd.classList.remove('flex')
+        sukses.classList.add('flex')
+        sukses.classList.remove('hidden')
+    })
+})
+
+payBtn.addEventListener('click',() => {
+    passwd.classList.remove('hidden')
+    passwd.classList.add('flex')
+});
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: ['index.html'],
+    theme: {
+      extend: {
+        colors: {
+          darkblue: "#25316D",
+          ijo: "#418C60",
+          
+          darkijo: "#3D6660",
+          ijoterang: "#66A399",
+          gelap: "#ob2027",
+          terang: "#8CBAB4",
+          navcolor : "#B3D1CD",
+        },
+      },
+    },
+    plugins: [],
+  }
